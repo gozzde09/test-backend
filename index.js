1ee60e5dc0c88d9e1fa153db7c69e257d1a21ea1
Gzd Akgn <gozzde0990@gmail.com>
2024-10-22 13:56:23 +0200

    cors

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express(),
   port = process.env.PORT || 3000;
+app.use(cors());
 
 app.get("/", (_request, response) => {
   response.send({ hello: "World" });
@@ -9,4 +10,11 @@ app.get("/", (_request, response) => {
 
 app.listen(port, () => {
   console.log(`Redo på http://localhost:${port}/`);
+});
+app.get("/products/:id", function (req, res, next) {
+  res.json({ msg: "This is CORS-enabled for all origins!" });
+});
+
+app.listen(80, function () {
+  console.log("CORS-enabled web server listening on port 80");
 });
